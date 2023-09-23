8b3f04633561c499b7f02089bb82cd39dcdfaaf5
егор <egorchik2006@bk.ru>
2023-09-23 16:14:32 +0300

    практика 5

---
+++ PZ_05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+            int i;
+
+            for (i = 0; <= 9; i = i + 1)
+                A[i] = i + 2;
+
+
+            Console.WriteLine("elements of arrey: ");
+
+            for (i = 0; i <= 9; i = i + 1)
+                Console.WriteLine(" A[{0}]={1 }", i, A[i]);
+        }
+    }
+}
