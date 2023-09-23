a1a2436b2dd1c11288cc2566035bd2643348483e
никуленков егор <knopka22820@bk.ru>
2023-09-24 01:13:22 +0300

    практика 5 хз

--- PZ_05/Program.cs
+++ PZ_05/Program.cs
@@ -4,17 +4,9 @@
     {
         static void Main(string[] args)
         {
-            int[] A = new int[10];
-            int i;
-
-            for (i = 0; <= 9; i = i + 1)
-                A[i] = i + 2;
-
-
-            Console.WriteLine("elements of arrey: ");
-
-            for (i = 0; i <= 9; i = i + 1)
-                Console.WriteLine(" A[{0}]={1 }", i, A[i]);
+            /*
+             *  не понял суть задания (условия задачи)
+             */
         }
     }
 }
