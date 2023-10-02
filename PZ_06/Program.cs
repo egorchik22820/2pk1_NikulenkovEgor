0cb8bb5bb0c9b104ffe08642c1e694687c8983db
egorchik22820 <egorchik2006@bk.ru>
2023-10-02 19:22:29 +0300

    PZ_06

---
+++ PZ_06/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] arr = new int[10];                       // создание массива
+             Random rand = new Random();            
+
+
+             for (int i = 0; i < arr.Length; i++)           // заполнение массива случайными числами
+             {
+                 arr[i] = rand.Next(0, 50);
+             }
+
+
+
+             for (int i = 0; i < arr.Length; i++)           // сортировка и вывод значений массива
+             {
+                int valmas = arr[i];                        // чтобы не менять значение в массиве
+
+                int sum = 0;
+
+                if (valmas > 9)                             // если число не однозначное , то
+                 {
+
+                    while (valmas > 0)                      // таким образом отделяем разряды
+                    {
+                        sum = sum + valmas % 10;
+                        valmas /= 10;
+                    }
+                 }
+                else sum = valmas;                          // иначе число записывает как сумму
+
+                Console.WriteLine(arr[i] + "\t" + sum);     // вывод
+            }
+        }
+
+
+    }
+}
