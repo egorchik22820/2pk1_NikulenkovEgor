4ef057b9bd245a4334d5ab490d6f4f85717f6a1a
egorchik22820 <egorchik2006@bk.ru>
2023-12-13 15:04:21 +0300

    gfg

---
+++ PZ_09/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            string maxString = "";
+            int maxLength = 0;
+
+            Console.WriteLine("Введите строки (для завершения введите 'stop'):");
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "stop")
+                    break;
+
+                if (input.Length > maxLength)
+                {
+                    maxLength = input.Length;
+                    maxString = input;
+                }
+            }
+
+            Console.WriteLine($"Строка максимальной длины: {maxString}");
+            Console.ReadLine();
+
+        }
+    }
+}
