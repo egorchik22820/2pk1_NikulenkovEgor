2b047e7e08c12d0740165a69c49a1c97b820fe36
egorchik22820 <143965345+egorchik22820@users.noreply.github.com>
2024-03-19 19:46:58 +0300

    Add files via upload

---
+++ 17-egorchik22820/consoleProject/Pasagir.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleProject
+{
+    internal class Pasagir : IPasagir
+    {
+        ParkingTicket ticket;
+        string Name { get; set; }
+        int Age { get; set; }
+        public Pasagir(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public void BuyTicket(ParkingTicket ticket)
+        {
+            this.ticket = ticket;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"name : {Name}");
+            Console.WriteLine($"age : {Age}");
+            Console.WriteLine($"ticket : {ticket.ID}");
+        }
+
+
+
+
+
+    }
+}
